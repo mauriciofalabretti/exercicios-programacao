9bf4d83771d68d9c7375b7b89a1d5d01a544a635
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-03 19:08:02 -0300

    Updating Python Exercises

---
+++ python/Desafios/Aula 10/Desafio031.py
@@ -0,0 +1,17 @@
+#Entrada e validação de dados pelo teclado com o tipo float
+liberado = False
+while liberado == False:
+    distancia = float(input('Informe a distância da viagem em Km: '))
+    if distancia <= 0:
+        print('Distância não pode ser igual ou menor a zero!')
+    else:
+        liberado = True
+
+
+#Condicional com o cálculo de preço da passagem baseado na distância da viagem
+if distancia <= 200:
+    preco_passagem = distancia * 0.50
+    print(f'Você irá pagar R$ {preco_passagem:.2f} pela passagem')
+else:
+    preco_passagem = distancia * 0.45
+    print(f'Você irá pagar R$ {preco_passagem:.2f} pela passagem')
