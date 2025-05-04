78790a1a30493a451c2ab323efee68f43fb1fd8b
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-04 17:07:17 -0300

    Update Desafio031.py

--- python/Desafios/Aula 10/Desafio031.py
+++ python/Desafios/Aula 10/Desafio031.py
@@ -11,7 +11,7 @@ while liberado == False:
 #Condicional com o cálculo de preço da passagem baseado na distância da viagem
 if distancia <= 200:
     preco_passagem = distancia * 0.50
-    print(f'Você irá pagar R$ {preco_passagem:.2f} pela passagem')
 else:
     preco_passagem = distancia * 0.45
-    print(f'Você irá pagar R$ {preco_passagem:.2f} pela passagem')
+
+print(f'Você irá pagar R$ {preco_passagem:.2f} pela passagem')
