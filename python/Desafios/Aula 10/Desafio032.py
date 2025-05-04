9ecd03ce3440bd7b1aa3fba7ff4c117d450c055b
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-04 17:26:43 -0300

    Update Desafio032.py

--- python/Desafios/Aula 10/Desafio032.py
+++ python/Desafios/Aula 10/Desafio032.py
@@ -1,10 +1,16 @@
-import calendar
+from calendar import isleap
+from datetime import datetime
+
 
 #Entrada de dados via teclado com o tipo inteiro e validada
 liberado = False
 while liberado == False:
-    ano = int(input('Digite o ano: '))
-    if ano <= 0:
+    ano = int(input('Digite o ano (coloque 0 para analisar ano atual): '))
+    if ano == 0:
+        ano_atual = datetime.now() #Atribuo a data local completa na variável ano_atual
+        ano = ano_atual.year #Acesso precisamente o ano através da variável ano_atual e atribuo ao ano
+        liberado = True
+    if ano < 0:
         print('Ano inválido! Tente novamente!')
     else:
         liberado = True
@@ -16,4 +22,4 @@ while liberado == False:
 #    print(f'O ano {ano} não é bissexto.')
 
 #Usando o módulo calendar
-print(f'{ano} é bissexto' if calendar.isleap(ano) == True else f'{ano} não é bissexto.')
+print(f'{ano} é bissexto' if isleap(ano) == True else f'{ano} não é bissexto.')
