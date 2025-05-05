4eae6eabb08f3167868d45336e340238b7f320bd
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-05 13:57:56 -0300

    Update Desafio02.py

--- python/Desafios/Aula 04/Desafio02.py
+++ python/Desafios/Aula 04/Desafio02.py
@@ -2,5 +2,5 @@ print('====== DESAFIO 02 ======')
 Dia = input('Dia = ')
 Mes = input('Mes = ')
 Ano = input('Ano = ')
-print(f'Você nasceu no dia {Dia} de {Mes} de {Ano}. Correto?')
+print(f'Você nasceu no dia {'\033[4;31m'}{Dia}{'\033[m'} de {'\033[4;31m'}{Mes}{'\033[m'} de {'\033[4;31m'}{Ano}{'\033[m'}. Correto?')
 
