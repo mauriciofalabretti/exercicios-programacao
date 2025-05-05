7e312beb728728ef1c7a4d2ece8275ad942029b7
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-05 14:37:31 -0300

    Update Desafio034.py

--- python/Desafios/Aula 10/Desafio034.py
+++ python/Desafios/Aula 10/Desafio034.py
@@ -8,11 +8,11 @@ while liberado == False:
         liberado = True
     except ValueError as e:
         if 'Salário_Inválido' in str(e):
-            print('ERRO! Salário deve ser maior que zero!')
+            print(f'{'\033[1;91m'}ERRO! Salário deve ser maior que zero!{'\033[m'}')
         else:
-            print('ERRO! Digite um valor númerico válido!')
+            print(f'{'\033[1;91m'}ERRO! Digite um valor númerico válido!{'\033[m'}')
     except Exception as e:
-        print(f'Erro inesperado! {e}')
+        print(f'{'\033[1;91m'}Erro inesperado!{'\033[m'} {e}')
 
 #Cálculo de aumento do salário
 if salario > 1250.00:
@@ -21,4 +21,4 @@ else:
     novo_salario = salario + (salario * 0.15)
 
 #Apresentação do salário pós-aumento
-print(f'O salário após o aumento será de R$ {novo_salario:.2f}')
+print(f'O salário após o aumento será de {'\033[4;92m'}R$ {novo_salario:.2f}{'\033[m'}')
