125aa39262a3a6d54b2f5243059b3a68d424fea5
Mauricio Falabretti <maurihbk@gmail.com>
2025-05-05 14:11:47 -0300

    Update Desafio029.py

--- python/Desafios/Aula 10/Desafio029.py
+++ python/Desafios/Aula 10/Desafio029.py
@@ -5,6 +5,6 @@ velocidade = float(input('Informe a velocidade: '))
 if velocidade > 80:
     valor_multa = (velocidade - 80) * 7 #Encontro o excesso de velocidade e calculo o valor da multa pelo valor de R$ 7,00
     print(f'Velocidade acima dos 80 Km/h permitidos!\n'
-          f'Você foi multado em: R$ {valor_multa:.2f}')
+          f'Você foi {'\033[1;91;107m'}multado em: R$ {valor_multa:.2f}{'\033[m'}')
 else:
-    print('Velocidade dentro do permitido.')
+    print(f'{'\033[0;32m'}Velocidade dentro do permitido{'\033[m'}.')
