015af5e43e8e04f421fe83bbed7d4b54ac422fd0
mauriciofalabretti <maurihbk@gmail.com>
2025-07-01 21:39:18 -0300

    adicionando o desafio097 da aula 20

---
+++ python/Desafios/Aula 20/Desafio097.py
@@ -0,0 +1,10 @@
+def escreva(msg: str):
+    print('~' * (len(msg) + 2))
+    print(f' {msg} ')
+    print('~' * (len(msg) + 2))
+
+
+# Programa principal
+escreva('AJ Styles')
+escreva('Curso de Python no YouTube')
+escreva('CeV')
